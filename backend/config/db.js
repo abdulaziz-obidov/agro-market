fc42d6ea667a3feaf1cd6f59c57c4e46749fea39
Abdulaziz Obidov <abdulaziz.obidov03@gamil.com>
2026-03-19 05:14:50 +0500

    chore: Neon Serverless Postgres DB ga o'tish (SSL sozlamalari yoqildi)

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,6 +4,12 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false, // SQl konsolga chiqishini oldini olish
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 
 const connectDB = async () => {
